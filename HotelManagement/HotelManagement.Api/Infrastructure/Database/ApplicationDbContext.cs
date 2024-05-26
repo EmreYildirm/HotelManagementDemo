e156bee12d7fe7f36c9482c79517c688c98b8113
emre.yildirim <emre.yildirim@a101.com.tr>
2024-05-26 14:44:21 +0300

    entities created, migration created, necessary dependencies injected.

---
+++ HotelManagement/HotelManagement.Api/Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.Api.Infrastructure.Database;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+
+    public DbSet<Domain.Hotel> Hotels { get; set; }
+    public DbSet<Domain.ContactInformation> ContactInformations { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Hotel>()
+            .HasMany(h => h.ContanInformation)
+            .WithOne(c => c.Hotel)
+            .HasForeignKey(c => c.HotelId);
+
+        modelBuilder.Entity<Domain.ContactInformation>()
+            .Property(c => c.InfoType)
+            .HasConversion<string>();
+    }
+}
---
+++ HotelManagement/HotelManagement.Api/ServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using HotelManagement.Api.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.Api;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddHotelApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient();
+
+        services.AddHttpContextAccessor();
+
+        services.AddDbContext<ApplicationDbContext>(
+            options => { options.UseNpgsql(configuration.GetConnectionString("DbConnection"), m => { m.EnableRetryOnFailure(); }); });
+
+        services.AddSwaggerGen(options => options.CustomSchemaIds(type => type.ToString()));
+        return services;
+    }
+
+    public static void UseService(this IApplicationBuilder app)
+    {
+        var serviceProvider = app.ApplicationServices;
+        using var serviceScope = serviceProvider.CreateScope();
+        var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+        context?.Database.Migrate();
+    }
+}
