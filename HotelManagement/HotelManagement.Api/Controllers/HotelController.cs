437e37130689b7b6ff4c3123143f03125d34f86b
emre.yildirim <emre.yildirim@a101.com.tr>
2024-05-26 16:17:39 +0300

    hotel adding feature completed. medaitr config added.

--- HotelManagement/HotelManagement.Api/Controllers/HotelController.cs
+++ HotelManagement/HotelManagement.Api/Controllers/HotelController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelManagement.Api.Features.Hotel;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
 
 namespace HotelManagement.Api.Controllers;
 
@@ -6,7 +8,17 @@ namespace HotelManagement.Api.Controllers;
 [Route("api/[controller]")]
 public class HotelController : ControllerBase
 {
-    public HotelController()
+    private readonly IMediator _mediator;
+
+    public HotelController(IMediator mediator)
     {
+        _mediator = mediator;
+    }
+
+    [HttpPost("add-hotel")]
+    [ProducesResponseType(typeof(Features.Hotel.Create.Response), 200)]
+    public async Task<Features.Hotel.Create.Response> AddProduct([FromBody] Features.Hotel.Create.Request request)
+    {
+        return await _mediator.Send(request);
     }
 }
---
+++ HotelManagement/HotelManagement.Api/Features/Hotel/Create.cs
@@ -0,0 +1,37 @@
+﻿using HotelManagement.Api.Features.Mapper;
+using HotelManagement.Api.Infrastructure.Database;
+using MediatR;
+
+namespace HotelManagement.Api.Features.Hotel;
+
+public class Create
+{
+    public record Request : IRequest<Response>
+    {
+        public string AuthorizedName { get; set; }
+        public string AuthorizedSurname { get; set; }
+        public string CompanyTitle { get; set; }
+    }
+
+    public class Response
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class Handler(ApplicationDbContext appDbContext) : IRequestHandler<Request, Response>
+    {
+        private readonly ApplicationDbContext _appDbContext = appDbContext;
+
+        public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var responseModel = new Response();
+
+            var entity = request.CreateRequestToHotel();
+
+            await _appDbContext.AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+            responseModel.Id = entity.Id;
+            return responseModel;
+        }
+    }
+}
